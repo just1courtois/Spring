c830085d8d59275103806f55ffe817ddb8008382
Maxence Caille <59600120+maxant38@users.noreply.github.com>
2021-11-16 09:22:11 +0100

    Ajout des commentaires

--- src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/HeaterDao.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+// Dao pour le model heater
+
 public interface HeaterDao extends JpaRepository<Heater,Long> , HeaterDaoCustom {
+
     @Modifying
     @Query
             ("delete from Heater h where h.room.id = (select r.id from h.room r where r.building.id = :id)")
