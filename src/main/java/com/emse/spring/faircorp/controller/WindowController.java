ec36d30b50ff21f8daf2ef9b96d96315ea80aff9
Maxence Caille <59600120+maxant38@users.noreply.github.com>
2021-11-16 09:18:37 +0100

    Ajout Commentaires

--- src/main/java/com/emse/spring/faircorp/controller/WindowController.java
+++ src/main/java/com/emse/spring/faircorp/controller/WindowController.java
@@ -13,38 +13,40 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Controller pour gérer les windows
+
 @CrossOrigin
-@RestController // (1)
-    @RequestMapping("/api/windows") // (2)
-    @Transactional // (3)
+@RestController 
+    @RequestMapping("/api/windows")
+    @Transactional 
     public class WindowController {
 
         private final WindowDao windowDao;
         private final RoomDao roomDao;
 
-        public WindowController(WindowDao windowDao, RoomDao roomDao) { // (4)
+        public WindowController(WindowDao windowDao, RoomDao roomDao) {
             this.windowDao = windowDao;
             this.roomDao = roomDao;
         }
 
-        @GetMapping // (5)
+        @GetMapping // J'obtiens la liste des windows existantes
         public List<WindowDto> findAll() {
-            return windowDao.findAll().stream().map(WindowDto::new).collect(Collectors.toList());  // (6)
+            return windowDao.findAll().stream().map(WindowDto::new).collect(Collectors.toList());  
         }
 
-        @GetMapping(path = "/{id}")
+        @GetMapping(path = "/{id}") // J'obtiens l'ensemble des informations concernant la window dont j'ai rentré l'id
         public WindowDto findById(@PathVariable Long id) {
-            return windowDao.findById(id).map(WindowDto::new).orElse(null); // (7)
+            return windowDao.findById(id).map(WindowDto::new).orElse(null); 
         }
 
-        @PutMapping(path = "/{id}/switch")
+        @PutMapping(path = "/{id}/switch") // Je change le statut de la window dont j'ai rentré l'id
         public WindowDto switchStatus(@PathVariable Long id) {
             Window window = windowDao.findById(id).orElseThrow(IllegalArgumentException::new);
             window.setWindowStatus(window.getWindowStatus() == WindowStatus.OPEN ? WindowStatus.CLOSED: WindowStatus.OPEN);
             return new WindowDto(window);
         }
 
-        @PostMapping // (8)
+        @PostMapping // Je crée une nouvelle room si une room avec l'id entré n'existe pas
         public WindowDto create(@RequestBody WindowDto dto) {
             // WindowDto must always contain the window room
             Room room = roomDao.getById(dto.getRoomId());
@@ -60,7 +62,7 @@ import java.util.stream.Collectors;
             return new WindowDto(window);
         }
 
-        @DeleteMapping(path = "/{id}")
+        @DeleteMapping(path = "/{id}") // Je supprime la window dont j'ai rentré l'id
         public void delete(@PathVariable Long id) {
             windowDao.deleteById(id);
         }
