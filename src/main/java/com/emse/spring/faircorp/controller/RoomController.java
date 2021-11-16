95701483a73fdaa84ee46e07c5ba9b63c6206a7e
Maxence Caille <59600120+maxant38@users.noreply.github.com>
2021-11-16 09:11:55 +0100

    Ajout des commentaires

--- src/main/java/com/emse/spring/faircorp/controller/RoomController.java
+++ src/main/java/com/emse/spring/faircorp/controller/RoomController.java
@@ -15,10 +15,12 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Controller pour gérer les rooms 
+
 @CrossOrigin
-@RestController // (1)
-@RequestMapping("/api/rooms") // (2)
-@Transactional // (3)
+@RestController 
+@RequestMapping("/api/rooms") 
+@Transactional 
 public class RoomController {
 
     private final RoomDao roomDao;
@@ -34,12 +36,12 @@ public class RoomController {
         this.heaterDao = heaterDao;
     }
 
-    @GetMapping
+    @GetMapping // J'obtiens l'ensemble des rooms existantes
     public List<RoomDto> findAll() {
-        return roomDao.findAll().stream().map(RoomDto::new).collect(Collectors.toList());  // (6)
+        return roomDao.findAll().stream().map(RoomDto::new).collect(Collectors.toList()); 
     }
 
-    @PostMapping // (8)  créer une room
+    @PostMapping // je créer une room si l'id rentré n'appartient pas à une room déjà existante
     public RoomDto create(@RequestBody RoomDto dto) {
         // RoomDto must always contain the room building
         Building building = buildingDao.getById(dto.getBuildingId());
@@ -53,45 +55,45 @@ public class RoomController {
         return new RoomDto(room);
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}") // J'obtiens l'ensemble des informations sur la room dont j'ai rentré l'id
     public RoomDto findById(@PathVariable Long id) {
         return roomDao.findById(id).map(RoomDto::new).orElse(null);
     }
 
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = "/{id}") // Je supprime la room dont j'ai rentré l'id ainsi que l'ensemble des heaters et windows contenus dans celle ci
     public void delete(@PathVariable Long id) {
         heaterDao.deleteAllHeatersInARoom(id);
         windowDao.deleteAllWindowsInARoom(id);
         roomDao.deleteById(id);
     }
 
-    @GetMapping(path="/{id}/heaters")
+    @GetMapping(path="/{id}/heaters") // J'obtiens l'ensemble des heaters contenus dans la room dont j'ai rentré l'id
     public List<HeaterDto> findAllHeaters(@PathVariable Long id) {
         List<Heater> heaters = roomDao.findRoomHeaters(id);
         return heaters.stream().map(HeaterDto::new).collect(Collectors.toList());
     }
 
-    @GetMapping(path="/{id}/windows")
+    @GetMapping(path="/{id}/windows") // J'obtiens l'ensemble des windows contenus dans la room dont j'ai rentré l'id
     public List<WindowDto> findAllWindows(@PathVariable Long id) {
         List<Window> windows = roomDao.findRoomWindows(id);
         return windows.stream().map(WindowDto::new).collect(Collectors.toList());
     }
 
 
-    @GetMapping(path="/{id}/switchWindows")
+    @GetMapping(path="/{id}/switchWindows") // Je modifie les status de l'ensemble des windows contenues dans la rooom dont j'ai rentré l'id
     public void switchWindowsStatus(@PathVariable Long id) {
         List<Window> windows = windowDao.findWindowsInARoom(id);
         windows.stream().forEach(window -> window.setWindowStatus(window.getWindowStatus() == WindowStatus.OPEN ? WindowStatus.CLOSED: WindowStatus.OPEN));
     }
 
-    @GetMapping(path="/{id}/switchHeaters")
+    @GetMapping(path="/{id}/switchHeaters")  // Je modifie les status de l'ensemble des heaters contenues dans la rooom dont j'ai rentré l'id
     public void switchHeatersStatus(@PathVariable Long id) {
         List<Heater> heaters = heaterDao.findHeatersInARoom(id);
         heaters.stream().forEach(heater -> heater.setHeaterStatus(heater.getHeaterStatus() == HeaterStatus.ON ? HeaterStatus.OFF: HeaterStatus.ON));
     }
     
-        //Update a room
-    @PutMapping(path = "/{id}")
+      
+    @PutMapping(path = "/{id}") // Je modifie la targer temperature de la room dont j'ai entré l'id
     public RoomDto updateTargetTemperature(@PathVariable Long id, @RequestBody RoomDto room) {
         Room original = roomDao.findById(id).orElseThrow(IllegalArgumentException::new);
 
