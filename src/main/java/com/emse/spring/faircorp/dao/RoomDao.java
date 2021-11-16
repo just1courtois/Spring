3484b2a0f624efb60b8bc008060948944ffab154
Maxence Caille <59600120+maxant38@users.noreply.github.com>
2021-11-16 09:27:43 +0100

    Ajout des commentaires

--- src/main/java/com/emse/spring/faircorp/dao/RoomDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/RoomDao.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+// Dao pour le model Room
+
 public interface RoomDao extends JpaRepository<Room, Long>, RoomDaoCustom {
     @Query("select r from Room r where r.name=:name")
     Room findByName(@Param("name") String name);
