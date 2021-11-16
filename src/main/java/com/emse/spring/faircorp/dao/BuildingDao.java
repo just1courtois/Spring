8f4b3548ec1e61ca63c8b3da7c430bade1f5dd7b
Maxence Caille <59600120+maxant38@users.noreply.github.com>
2021-11-16 09:19:37 +0100

    Ajout des commentaires

--- src/main/java/com/emse/spring/faircorp/dao/BuildingDao.java
+++ src/main/java/com/emse/spring/faircorp/dao/BuildingDao.java
@@ -4,6 +4,7 @@ import com.emse.spring.faircorp.model.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+// Dao pour le model building
 
 public interface BuildingDao extends JpaRepository<Building, Long>, BuildingDaoCustom {
     @Query("select b from Building b where b.name=:name")
